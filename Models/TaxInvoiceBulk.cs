4078a8a6174af3ca85d3889c729f08eb1508a6d6
Martinus Suherman <martinus.suherman@gmail.com>
2025-01-22 15:08:58 +0700

    Update TaxInvoiceBulk
    - add read-only property Attr to set noNamespaceSchemaLocation

--- Models/TaxInvoiceBulk.cs
+++ Models/TaxInvoiceBulk.cs
@@ -1,7 +1,23 @@
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
 namespace CoreTaxXmlWriter.Models;
 
 public class TaxInvoiceBulk
 {
+    [XmlAttribute("noNamespaceSchemaLocation", Namespace = XmlSchema.InstanceNamespace)]
+    public string Attr
+    {
+        get
+        {
+            return "TaxInvoice.xsd";
+        }
+        set
+        {
+            throw new NotSupportedException();
+        }
+    }
+
     public string TIN { get; set; } = string.Empty;
 
     public List<TaxInvoice> ListOfTaxInvoice { get; } = [];
